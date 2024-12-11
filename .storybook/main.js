6977166d3e386d1493618c18a3a6453de17a3b30
muse9312 <muse9312@naver.com>
2024-12-11 09:26:00 +0900

    storybook.main modify

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,28 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 
 const config = {
-    stories: ['../src/Docs.stories.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-onboarding',
-        '@storybook/addon-interactions',
-        '@storybook/addon-storysource',
-    ],
-    framework: {
-        name: '@storybook/react-vite',
-        options: {},
-    },
-    docs: {
-        autodocs: 'tag',
-    },
-    core: {
-        builder: '@storybook/builder-vite',
-    },
-    staticDirs: ['..\\public'],
+  stories: [
+    "../src/Docs.stories.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  staticDirs: ["../public"],
 };
 export default config;
